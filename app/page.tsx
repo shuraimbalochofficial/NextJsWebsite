0645ced880fb5a27e6f57bb41cac53ed1ccae854
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-05-22 23:56:52 +0500

    Navbar + Working on event Card

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,23 @@ const page = () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub For Every DEV Event <br />  You Must&apos;nt MISS
+        The Hub For Every DEV Event <br /> You Must&apos;nt MISS
       </h1>
       <p className="text-center mt-5">
         Hackathon, Meet&Greet, Conferences, All in one place
       </p>
 
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
