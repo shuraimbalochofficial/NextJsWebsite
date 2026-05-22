1a9644f1f3ad5a72ea4474918b3e9416d4d65307
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-05-23 02:50:42 +0500

    Working on API

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,12 +1,35 @@
+import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import Event from "@/database/event.model";
 
 export async function POST(req: NextRequest) {
   try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        {
+          message: "Invalid JSON form data",
+        },
+        { status: 400 },
+      );
+    }
+
+    const createdEvent = await Event;
   } catch (e) {
     console.log(e);
-    return NextResponse.json({
-      message: "Event Creation Failed",
-      error: e instanceof Error ? e.message : "Unknown error",
-    });
+    return NextResponse.json(
+      {
+        message: "Event Creation Failed",
+        error: e instanceof Error ? e.message : "Unknown error",
+      },
+      { status: 500 },
+    );
   }
 }
