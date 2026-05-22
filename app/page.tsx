ed2f371f691fa0cafd9243f82ed00680a5ca4fc7
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-05-22 23:09:26 +0500

    Building Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
+import ExploreBtn from "@/components/ExploreBtn";
 import React from "react";
 
 const page = () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub For Every DEV <br /> Event You Must&apos;t MISS
+        The Hub For Every DEV Event <br />  You Must&apos;nt MISS
       </h1>
       <p className="text-center mt-5">
         Hackathon, Meet&Greet, Conferences, All in one place
       </p>
+
+      <ExploreBtn />
     </section>
   );
 };
