a189d5c2ef2ff48d3d587c5a9bebe4c4608e7464
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-05-23 23:46:42 +0500

    Api Slug Page

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,13 @@ const page = async () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          { events && events.length > 0 && events.map((event : IEvent) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
