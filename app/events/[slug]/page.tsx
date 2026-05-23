88d6fafcb91c0e448a0bfdffd78c11e67521d459
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-05-24 00:37:02 +0500

    Working On Events Detail Page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import { notFound } from "next/navigation";
 import React from "react";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const EventDetailsPage = async ({
   params,
@@ -6,6 +9,10 @@ const EventDetailsPage = async ({
   params: Promise<{ slug: string }>;
 }) => {
   const { slug } = await params;
+  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event } = await request.json();
+
+  if (!event) return notFound();
   return (
     <section id="event">
       <h1>
